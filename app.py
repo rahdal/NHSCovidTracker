054a83d2ca817d8226f2f85e6faacce75ab02eb3
rahdal <rdalvi2014@gmail.com>
2022-04-17 17:09:58 -0400

    Moving average added

--- app.py
+++ app.py
@@ -3,6 +3,7 @@ import pandas as pd
 import json
 import plotly
 import plotly.express as px
+import plotly.graph_objects as go
 import bovid
 
 
@@ -13,7 +14,9 @@ app.static_folder = 'static'
 def index():
 
     df = bovid.df
+    df['MA7'] = df.Cases.rolling(7).mean()
     fig = px.bar(df, x='Date', y='Cases', template='plotly_dark')
+    fig.add_traces(go.Scatter(x= df.Date, y=df.MA7, mode = 'lines', name = '7 Day Average'))
     graphJSON = json.dumps(fig, cls=plotly.utils.PlotlyJSONEncoder)
 
     return render_template('index.html', graphJSON = graphJSON)
